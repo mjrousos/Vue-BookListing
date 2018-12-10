0a9dac242f74fb798aa035de22292b51af1569bb
Mike Rousos <Mike@MJRousos.com>
2018-12-10 17:24:38 -0500

    Add component for adding new books

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -5,10 +5,7 @@
 <script>
 export default {
   name: 'BookItem',
-  props: ['book'],
-  data() {
-
-  }
+  props: ['book']
 }
 </script>
 
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,15 +4,19 @@
     <ul>
       <book-item v-for="book in books" :book='book' />
     </ul>
+    <hr>
+    <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
   name: 'BookList',
   components: {
+    BookForm,
     BookItem
   },
   data() {
@@ -24,6 +28,11 @@ export default {
           {title: 'American Gods', author: 'Neil Gaiman'},
           {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
       ]
+    }
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor});
     }
   }
 }
